7962b663d2edfe1706d833544360e7a54676679d
Mohamed-Ashraf273 <ma2736666@gmail.com>
2026-06-11 21:32:34 +0300

    restore health route

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 
 from fastapi import FastAPI
 
+from backend.api.routes import router as sessions_router
 from backend.api.whatsapp import router as whatsapp_router
 
 logging.basicConfig(level=logging.INFO)
@@ -9,4 +10,5 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+app.include_router(sessions_router)
 app.include_router(whatsapp_router)
