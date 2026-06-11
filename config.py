e442e41902bd4b96526af3441d4e051156729efc
Mohamed-Ashraf273 <ma2736666@gmail.com>
2026-06-11 21:42:50 +0300

    fix config to use os.getenv for Render env vars

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+import os
 import dotenv
 
 dotenv.load_dotenv()
 
 class Config:
-    ACCESS_TOKEN = dotenv.get_key(".env", "ACCESS_TOKEN")
-    PHONE_NUMBER_ID = dotenv.get_key(".env", "PHONE_NUMBER_ID")
-    WEBHOOK_VERIFY_TOKEN = dotenv.get_key(".env", "WEBHOOK_VERIFY_TOKEN")
-    WHATSAPP_API_URL = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+    WEBHOOK_VERIFY_TOKEN = os.getenv("WEBHOOK_VERIFY_TOKEN")
+    WHATSAPP_API_URL = f"https://graph.facebook.com/v25.0/{os.getenv('PHONE_NUMBER_ID')}/messages"
 
 
 config = Config()
